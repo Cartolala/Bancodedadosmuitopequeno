498b7c3e01feb9285c11b2ef8689b6cacd1661c2
Cartolala <oficiodoossos96@gmail.com>
2022-10-01 21:52:45 -0300

    banco de dados

---
+++ MiniMuitoPequenoBancodeDados/MainForm.Designer.cs
@@ -0,0 +1,299 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mathe
+ * Date: 20/09/2022
+ * Time: 23:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace MiniMuitoPequenoBancodeDados
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(99, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Registro";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(117, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "0";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(11, 32);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Nome";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(11, 58);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 23);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Endereço";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(11, 84);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 23);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "Salario";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(117, 32);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(183, 20);
+			this.textBox1.TabIndex = 5;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(117, 58);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(183, 20);
+			this.textBox2.TabIndex = 6;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(117, 84);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 7;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(34, 172);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(57, 44);
+			this.button1.TabIndex = 8;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.Transparent;
+			this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+			this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Location = new System.Drawing.Point(97, 172);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(57, 44);
+			this.button2.TabIndex = 9;
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.Transparent;
+			this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+			this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button3.FlatAppearance.BorderSize = 0;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Location = new System.Drawing.Point(160, 172);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(57, 44);
+			this.button3.TabIndex = 10;
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(223, 185);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(37, 20);
+			this.textBox4.TabIndex = 11;
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.Transparent;
+			this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+			this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button4.FlatAppearance.BorderSize = 0;
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.Location = new System.Drawing.Point(271, 172);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(55, 44);
+			this.button4.TabIndex = 12;
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.richTextBox1.Location = new System.Drawing.Point(12, 255);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(340, 178);
+			this.richTextBox1.TabIndex = 13;
+			this.richTextBox1.Text = "";
+			// 
+			// button5
+			// 
+			this.button5.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.button5.Location = new System.Drawing.Point(-119, 332);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(21, 23);
+			this.button5.TabIndex = 15;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Visible = false;
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.Transparent;
+			this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+			this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button6.FlatAppearance.BorderSize = 0;
+			this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button6.Location = new System.Drawing.Point(91, 32);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(20, 20);
+			this.button6.TabIndex = 16;
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.Visible = false;
+			this.button6.Click += new System.EventHandler(this.Button6Click);
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.Transparent;
+			this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+			this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button7.FlatAppearance.BorderSize = 0;
+			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button7.Location = new System.Drawing.Point(306, 32);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(20, 18);
+			this.button7.TabIndex = 17;
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.Click += new System.EventHandler(this.Button7Click);
+			// 
+			// button8
+			// 
+			this.button8.BackColor = System.Drawing.Color.Transparent;
+			this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+			this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button8.FlatAppearance.BorderSize = 0;
+			this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button8.Location = new System.Drawing.Point(332, 31);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(20, 20);
+			this.button8.TabIndex = 18;
+			this.button8.UseVisualStyleBackColor = false;
+			this.button8.Visible = false;
+			this.button8.Click += new System.EventHandler(this.Button8Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(361, 475);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.MinimumSize = new System.Drawing.Size(377, 350);
+			this.Name = "MainForm";
+			this.Text = "MiniMuitoPequenoBancodeDados";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ MiniMuitoPequenoBancodeDados/MainForm.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MiniMuitoPequenoBancodeDados
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+
+            InitializeComponent();
+            
+            // os comando comentado estao ai para me lembra
+
+        }
+        
+        // Inicia variaveis para multiplas pesquisas
+        string[][] bufferdados = new string[128][];
+        int o = 0;
+        int q = 0;
+        
+        void Limpar() 
+        {
+            // Reinicia a parte do programa para uma nova entrada de dado
+            o = 0;
+            button8.Visible = false;
+            button6.Visible = false;
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            label2.Text = richTextBox1.Lines.Length.ToString();
+        }
+        
+        // Funcao para limpar o bufferdados caso um bug aconteça
+        void NulllArray(Array[] arrayx)
+        {
+            // Muda todos os valores de um array para null
+            // Para quando acha um valor null
+            for (int i = 0; i < arrayx.Length; i++) {
+                if(arrayx[i] != null){
+                    arrayx[i] = null;
+                } else {
+                    return;
+                }
+            }
+        }
+        
+        void Button1Click(object sender, EventArgs e)
+        {
+            // Botao Novo
+            // Limpa os campos para uma nova entrada
+            Limpar();
+        }
+        
+        void Button2Click(object sender, EventArgs e)
+        {
+            // Botao Salvar
+            // Salva para o 'banco de dado'
+            // Caso ocorra uma pesquisa, mudara no banco
+            
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                string linhaNova = textBox1.Text+"\t"+textBox2.Text+"\t"+textBox3.Text+"\t"+label2.Text;
+                
+                int numLinha = int.Parse(label2.Text);
+                
+                if (label2.Text == richTextBox1.Lines.Length.ToString())
+                {
+                    richTextBox1.Text = richTextBox1.Text+'\n'+linhaNova;
+                    richTextBox1.SaveFile("funcionarios.txt", RichTextBoxStreamType.PlainText);
+                    MessageBox.Show("Salvou");
+                    Limpar();
+                } else {
+                    string linhaVelha = richTextBox1.Lines[numLinha];
+                    richTextBox1.Text = richTextBox1.Text.Replace(linhaVelha, linhaNova);
+                    richTextBox1.SaveFile("funcionarios.txt", RichTextBoxStreamType.PlainText);
+                    MessageBox.Show("Mudou");
+
+                }
+            } else {
+                MessageBox.Show("Campos Vazios");
+            }
+        }
+        
+        void Button3Click(object sender, EventArgs e)
+        {
+            // Botao Pesquisar
+            // Pesquisa com o numero do textbox4 a linha do banco de dado 
+            
+            int numLinha;
+            
+            if (!int.TryParse(textBox4.Text, out numLinha))
+            {
+                MessageBox.Show("Não é um numero");
+                return;
+            }
+            if (numLinha >= richTextBox1.Lines.Length || numLinha == 0)
+            {
+                MessageBox.Show("Não existe");
+                return;
+            }
+            
+            label2.Text = numLinha.ToString();
+            
+            string[] dado = richTextBox1.Lines[numLinha].Split('\t');
+            textBox1.Text = dado[0];
+            textBox2.Text = dado[1];
+            textBox3.Text = dado[2];
+            
+        }
+        
+        void Button4Click(object sender, EventArgs e)
+        {
+            // Botao Deletar
+            // Deleta a linha do banco de dados com o numero fornecido do textbox4
+            
+            int numLinha;
+            if (!int.TryParse(textBox4.Text, out numLinha))
+            {
+                MessageBox.Show("Nao é um numero");
+                return;
+            }
+            
+            if (numLinha >= richTextBox1.Lines.Length)
+            {
+                MessageBox.Show("Nao existe");
+                return;
+            }
+                            
+            int maxlinha = richTextBox1.Lines.Length-1;
+            
+            if (numLinha > maxlinha || numLinha == 0){
+                MessageBox.Show("Nao existe");
+                return;
+            }
+            
+            if (numLinha == maxlinha){
+                string linhaVelha = richTextBox1.Lines[numLinha];
+                richTextBox1.Text = richTextBox1.Text.Replace(linhaVelha, null);
+                
+                string linhaAnteriorOld = richTextBox1.Lines[numLinha-1];
+                
+                // Exclui o NewLine da penúltima linha
+                string linhaAnteriorNew = linhaAnteriorOld.Replace('\n', ' ');
+                richTextBox1.Text = richTextBox1.Text.Replace(linhaAnteriorOld + '\n', linhaAnteriorNew);
+                richTextBox1.SaveFile("funcionarios.txt", RichTextBoxStreamType.PlainText);
+                MessageBox.Show("Apagou");
+            } else {
+                string linhaVelha = richTextBox1.Lines[numLinha];
+                richTextBox1.Text = richTextBox1.Text.Replace(linhaVelha + '\n', null);
+                richTextBox1.SaveFile("funcionarios.txt", RichTextBoxStreamType.PlainText);
+                MessageBox.Show("Apagou");
+            }        
+        }
+        
+//        void Button5Click(object sender, EventArgs e)
+//        {
+            // Ignorar, funçao de pesquisa, mas so retorna o primero resultado que encontrar
+//            
+//            int maxLinha = richTextBox1.Lines.Length;
+//            
+//            if (textBox1.Text == ""){
+//                return;
+//            }
+//            
+//            for (int i = 1; i < maxLinha; i++) {
+//                string[] compare = richTextBox1.Lines[i].Split('\t');
+//                
+//                if (compare[0].ToLower() == textBox1.Text.ToLower()){
+//                    textBox2.Text = compare[1];
+//                    textBox3.Text = compare[2];
+//                    return;
+//                }
+//            }
+//        }
+        
+        void Button7Click(object sender, EventArgs e)
+        {
+            // Botao Pesquisar
+            // Pesquisa por nomes com a string fornecida do textbox1
+            // Se tiver mais que um resultado, habilita os botoes 6 e 8
+            
+            // Reinicia para uma nova pesquisa
+            o=0; q=0;
+            button8.Visible = false;
+            button6.Visible = false;
+            NulllArray(bufferdados);
+            
+            int maxLinha = richTextBox1.Lines.Length;
+            
+            if (textBox1.Text == ""){
+                MessageBox.Show("Campo Vazio");
+                return;
+            }
+            
+            for (int i = 1; i < maxLinha; i++) {
+                string[] compare = richTextBox1.Lines[i].Split('\t');
+                
+                // Usa Expressao Regular para tornar a pesquisa mais generica
+                Regex regex = new Regex(textBox1.Text.ToLower());
+                
+                if (regex.IsMatch(compare[0].ToLower())){
+                    bufferdados[o] = compare;
+                    bufferdados[o][3] = i.ToString();
+//                    MessageBox.Show(string.Concat(bufferdados[o])); Caso seja necessario o debug
+                    o++;
+                }
+                
+//                if (compare[0].ToLower() == textBox1.Text.ToLower()){
+//                    
+//                    bufferdados[o] = compare;
+//                    bufferdados[o][3] = i.ToString();
+////                    MessageBox.Show(string.Concat(bufferdados[o])); Caso seja necessario o debug
+//                    o++;    
+//                }
+            }
+            
+            if(bufferdados[0] == null){
+                MessageBox.Show("Nao encontrado");
+                return;
+            }
+            
+            if(o > 1){
+                MessageBox.Show(o.ToString() + " Resultados Foram Encontrados");
+                button8.Visible = true;
+            } else {
+                MessageBox.Show(o.ToString() + " Resultado Foi Encontrado");
+            }
+            
+            textBox1.Text = bufferdados[0][0];
+            textBox2.Text = bufferdados[0][1];
+            textBox3.Text = bufferdados[0][2];
+            label2.Text   = bufferdados[0][3];
+        }
+        
+        void Button6Click(object sender, EventArgs e)
+        {
+            // Botao Voltar
+            // retrocede o index, mostrando o resultado anterior
+            
+            button8.Visible = true;
+            
+            q--;
+            textBox1.Text = bufferdados[q][0];
+            textBox2.Text = bufferdados[q][1];
+            textBox3.Text = bufferdados[q][2];
+            label2.Text   = bufferdados[q][3];
+            
+            if (q == 0){
+                button6.Visible = false;
+                return;
+            }
+        }
+        
+        void Button8Click(object sender, EventArgs e)
+        {
+            // Botao avançar
+            // avança o index, mostrando o proximo resultado
+            
+            button6.Visible = true;
+            
+            q++;
+            textBox1.Text = bufferdados[q][0];
+            textBox2.Text = bufferdados[q][1];
+            textBox3.Text = bufferdados[q][2];
+            label2.Text   = bufferdados[q][3];
+            
+            if (q == o - 1){
+                button8.Visible = false;
+                return;
+            }
+        }
+        
+        void MainFormLoad(object sender, EventArgs e)
+        {
+            try {
+                richTextBox1.LoadFile("funcionarios.txt", RichTextBoxStreamType.PlainText);
+            } catch (IOException exc) {
+                richTextBox1.SaveFile("funcionarios.txt", RichTextBoxStreamType.PlainText);
+                MessageBox.Show("existe nao irmao, calma que eu crio");
+            }
+            
+            int maxlinha = richTextBox1.Lines.Length;
+            label2.Text = maxlinha.ToString();
+        }
+    }
+}
